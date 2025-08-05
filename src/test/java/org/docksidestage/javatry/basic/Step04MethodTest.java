daf0f2633a3590a651ffc308650a216f99fe93c3
a.kumoshita <a.kumoshita@bizreach.co.jp>
2025-08-05 15:57:30 +0900

    Step04の完了・提出

--- src/test/java/org/docksidestage/javatry/basic/Step04MethodTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step04MethodTest.java
@@ -22,7 +22,7 @@ import org.docksidestage.unit.PlainTestCase;
  * Operate exercise as javadoc. If it's question style, write your answer before test execution. <br>
  * (javadocの通りにエクササイズを実施。質問形式の場合はテストを実行する前に考えて答えを書いてみましょう)
  * @author jflute
- * @author your_name_here
+ * @author Kumoshita-a
  */
 public class Step04MethodTest extends PlainTestCase {
 
@@ -35,7 +35,8 @@ public class Step04MethodTest extends PlainTestCase {
      */
     public void test_method_call_basic() {
         String sea = supplySomething();
-        log(sea); // your answer? =>
+        log(sea); // your answer? => over
+        // seaはsupplySomething()メソッドの戻り値である"over"が代入される。
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -43,28 +44,34 @@ public class Step04MethodTest extends PlainTestCase {
         String sea = functionSomething("mystic");
         consumeSomething(supplySomething());
         runnableSomething();
-        log(sea); // your answer? => 
+        log(sea); // your answer? => mysmys
+        // seaはfunctionSomething("mystic")の戻り値であるちticがmisに変換された"mysmys"が代入される。
+        // consumeSomething, runnableSomethingは呼び出されるが、seaの値には影響しない。
     }
 
     private String functionSomething(String name) {
         String replaced = name.replace("tic", "mys");
         log("in function: {}", replaced);
         return replaced;
+        // ticをmysに置き換えた文字列を返すメソッド
     }
 
     private String supplySomething() {
         String sea = "over";
         log("in supply: {}", sea);
         return sea;
+        // overという文字列を返すメソッド
     }
 
     private void consumeSomething(String sea) {
         log("in consume: {}", sea.replace("over", "mystic"));
+        // logを出すだけ
     }
 
     private void runnableSomething() {
         String sea = "outofshadow";
         log("in runnable: {}", sea);
+        // logを出すだけ
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -76,7 +83,15 @@ public class Step04MethodTest extends PlainTestCase {
         if (!land) {
             sea = sea + mutable.getStageName().length();
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 910
+        // mutableはSt4MutableStageのインスタンスである。
+        // helloMutableメソッドを呼び出すと、seaはsea - 4の値である900が渡され、landはfalseが渡される。
+        // helloMutableメソッド内でseaはインクリメントされて901になり、landはtrueとなっているが、参照渡しのため、元のlandには影響しない。
+        // piariにはもとのmutableが渡されているため、helloMutableメソッド内での変更が反映される。
+        // したがって、setStageName("mystic")が呼び出され、mutableのstageNameは"mystic"に変更される。
+        // その後、if文の条件はlandがfalseなのでtrueとなり、seaはsea + mutable.getStageName().length()が実行される。
+        // つまり、seaは904 + "mystic".length() = 904 + 6 = 910となる。
+        // したがって、最終的にseaには910が代入される
     }
 
     private int helloMutable(int sea, Boolean land, St4MutableStage piari) {
@@ -115,12 +130,16 @@ public class Step04MethodTest extends PlainTestCase {
         }
         ++sea;
         sea = inParkCount;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 100
+        // inParkCountは初期値0で、offAnnualPassportではhasAnnualPassportに影響はないので、hasAnnualPassportはtrueのままである。
+        // よって、goToPark()メソッドがfor文で100回呼び出されるので、inParkCountは100回インクリメントされる。
+        // その後、++seaでseaは1増えて1となるが、最後にsea = inParkCount;でinParkCountの値が再代入されるため、最終的にseaには100が代入される。
     }
 
     private void offAnnualPassport(boolean hasAnnualPassport) {
         hasAnnualPassport = false;
     }
+    // ここのhasAnnualPassportはprivateインスタンス変数ではなく、メソッドの引数なので、インスタンス変数には影響しない。（名前が一緒だからややこしいやつ）
 
     private void goToPark() {
         if (hasAnnualPassport) {
@@ -152,12 +171,34 @@ public class Step04MethodTest extends PlainTestCase {
      */
     public void test_method_making() {
         // use after making these methods
-        //String replaced = replaceCwithB(replaceAwithB("ABC"));
-        //String sea = quote(replaced, "'");
-        //if (isAvailableLogging()) {
-        //    showSea(sea);
-        //}
+        String replaced = replaceCwithB(replaceAwithB("ABC"));
+        String sea = quote(replaced, "'");
+        if (isAvailableLogging()) {
+            showSea(sea);
+        }
     }
 
     // write methods here
+
+    private final boolean availableLogging = true;
+
+    private String replaceAwithB(String str) {
+        return str.replace("A", "B");
+    }
+
+    private String replaceCwithB(String str) {
+        return str.replace("C", "B");
+    }
+
+    private String quote(String str, String quotation) {
+        return (quotation + str + quotation);
+    }
+
+    private boolean isAvailableLogging() {
+        return availableLogging;
+    }
+
+    private void showSea(String sea) {
+        log(sea);
+    }
 }
